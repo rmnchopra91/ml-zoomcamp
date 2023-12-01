8671c396980e82e0c583c7202ae8eb520fd3e909
Raman <rmnchopra91@gmail.com>
2023-12-01 10:52:00 +0530

    updated homework

--- homework/2023/10-kubernetes/q6_test.py
+++ homework/2023/10-kubernetes/q6_test.py
@@ -1,7 +1,13 @@
 import requests
+from time import sleep
 
 url = "http://localhost:9696/predict"
 client = {"job": "retired", "duration": 445, "poutcome": "success"}
 print("request ready for process")
 response = requests.post(url, json=client).json()
 print(f"{response}")
+
+while True:
+    sleep(0.1)
+    response = requests.post(url, json=client).json()
+    print(response)
